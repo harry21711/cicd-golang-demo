a83e34eba25f938b59789f83ecf8846bd8281cbd
harry21711 <34978618+harry21711@users.noreply.github.com>
2023-11-22 19:30:06 +0530

    Update main.go only env

--- main.go
+++ main.go
@@ -9,26 +9,8 @@ import (
 )
 
 func main() {
-
-	fmt.Println("Start main")
-	buildNumber := os.Getenv("BUILD_NUMBER")
-	fmt.Println("Start End")
-	log.Printf("drift type: %s", buildNumber)
-	fmt.Println("buildNumber : ", buildNumber)
-	fmt.Println("Start End")
-
-	// r := mux.NewRouter()
-	// r.HandleFunc("/", Output)
-	// http.Handle("/", r)
-
-	// log.Fatal(http.ListenAndServe(":9000", r))
+	for _, env := range os.Environ() {
+		fmt.Println(env)
+	}
 }
 
-func Output(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "Random no : %v", randomNumbers())
-}
-
-func randomNumbers() int {
-	return rand.Intn(1000)
-}
