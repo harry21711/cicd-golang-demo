bea6e697de52ba8c774fef82002247919b060e6c
harry21711 <34978618+harry21711@users.noreply.github.com>
2023-11-22 20:25:40 +0530

    Update main.go final comment

--- main.go
+++ main.go
@@ -13,5 +13,20 @@ func main() {
 	fmt.Println("end")
 	fmt.Println(os.Getenv("BU"))
 	fmt.Println("final end")
+
+	comment := "Update configs"
+	var a, b string = "59", "https"
+	buildNo := os.Getenv("BN")
+	buildURL := os.Getenv("BU")
+	
+	if buildNo != "" {
+		comment = comment + "\nbuild no : " + buildNo
+	}
+
+	if buildURL != "" {
+		comment = comment + "\nbuild job url : " + buildURL
+	}
+
+	fmt.Println(comment)
 }
 
