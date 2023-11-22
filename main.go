a1f31ffaeaab9a14ba45bc93fed1956fe751212b
harry21711 <34978618+harry21711@users.noreply.github.com>
2023-11-22 19:39:04 +0530

    Update main.go part

--- main.go
+++ main.go
@@ -6,8 +6,12 @@ import (
 )
 
 func main() {
+	fmt.Println("start")
 	for _, env := range os.Environ() {
 		fmt.Println(env)
 	}
+	fmt.Println("end")
+	fmt.Println(os.Getenv("BU"))
+	fmt.Println("final end")
 }
 
